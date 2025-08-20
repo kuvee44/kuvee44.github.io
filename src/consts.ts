999d30f4895ede2197c1a599be775340d9d6ab99
Phuoc Loi <tranphuocloi.hp18@gmail.com>
2025-08-20 15:04:48 +0700

    up-loadwriteup

--- src/consts.ts
+++ src/consts.ts
@@ -3,8 +3,8 @@ import type { IconMap, SocialLink, Site } from '@/types'
 export const SITE: Site = {
   title: 'kuvee',
   description:
-    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  href: 'https://astro-erudite.vercel.app',
+    "Hi! I'm Kuvee. This is my personal blog where I share my thoughts, writeups, and research on cybersecurity. Most posts focus on CTF, especially pwn – my favorite field. Hope you find something useful here ^^",
+  href: 'https://kuvee44.github.io',
   author: 'kuvee',
   locale: 'VietNam',
   featuredPostCount: 2,
@@ -12,37 +12,16 @@ export const SITE: Site = {
 }
 
 export const NAV_LINKS: SocialLink[] = [
-  {
-    href: '/blog',
-    label: 'blog',
-  },
-  {
-    href: '/authors',
-    label: 'authors',
-  },
-  {
-    href: '/about',
-    label: 'about',
-  },
+  { href: '/blog', label: 'blog' },
+  { href: '/authors', label: 'authors' },
+  { href: '/about', label: 'about' },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
-  {
-    href: 'https://github.com/jktrn',
-    label: 'GitHub',
-  },
-  {
-    href: 'https://twitter.com/enscry',
-    label: 'Twitter',
-  },
-  {
-    href: 'mailto:jason@enscribe.dev',
-    label: 'Email',
-  },
-  {
-    href: '/rss.xml',
-    label: 'RSS',
-  },
+  { href: 'https://github.com/jktrn', label: 'GitHub' },
+  { href: 'https://twitter.com/enscry', label: 'Twitter' },
+  { href: 'mailto:tranphuocloi.hp18@gmail.com', label: 'Email' }, // sửa ở đây
+  { href: '/rss.xml', label: 'RSS' },
 ]
 
 export const ICON_MAP: IconMap = {
