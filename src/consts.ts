b382b0fdda4551ba927bc15e427ed03d777b72ad
Phuoc Loi <tranphuocloi.hp18@gmail.com>
2025-12-29 21:57:45 +0700

    new

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { IconMap, SocialLink, Site } from '@/types'
 export const SITE: Site = {
   title: 'kuvee',
   description:
-    "Hi! I'm Kuvee. This is my personal blog where I share my thoughts, writeups, and research on cybersecurity. Most posts focus on CTF, especially pwn – my favorite field. Hope you find something useful here ^^",
+    "I'm Kuvee, and I enjoy solving pwn challenges. This blog is where I share my writeups and research.",
   href: 'https://kuvee44.github.io',
   author: 'kuvee',
   locale: 'VietNam',
@@ -14,7 +14,6 @@ export const SITE: Site = {
 export const NAV_LINKS: SocialLink[] = [
   { href: '/blog', label: 'blog' },
   { href: '/authors', label: 'authors' },
-  { href: '/about', label: 'about' },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
